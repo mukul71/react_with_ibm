90df769b4996a48b7dcd6bd77f14c6368b6b928c
mukul <mukul71@gmail.com>
2024-04-17 12:43:21 +0600

    correction maee

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -1,13 +1,9 @@
 //import logo from './logo.svg';
 import './App.css';
-function App(props){
-  const currentDate = new Date();
 
+function App (){
   return(
-    <div>>
     <h1>Hello World!</h1>
-    <h2>The time now is {currentDate.toLocaleDateString()}.</h2>
-    </div>
   );
 }
 export default App;
