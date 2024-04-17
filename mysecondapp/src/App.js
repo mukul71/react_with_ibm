fdc49628fdf1bb8a6e1ba240382fb49b4b090d77
mukul <mukul71@gmail.com>
2024-04-17 13:03:41 +0600

    toLocalTimeString() added

--- mysecondapp/src/App.js
+++ mysecondapp/src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App(){
+function App(props){
+  const currTime = new Date();
+
   return(
+    <div>
     <h1>Hello Mukul, this is your second app.</h1>
+    <h2>The time now is {currTime.toLocaleTimeString()}.</h2>
+    </div>
   );
 }
 export default App;
