5394278975abea85704a77cf6961a615b69ba8c7
mukul <mukul71@gmail.com>
2024-04-17 12:10:03 +0600

    time method added to display time

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -1,8 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
-function App(){
+function App(props){
+  const currentDate = new Date();
+
   return(
+    <div>>
     <h1>Hello World!</h1>
+    <h2>The time now is {currentDate.toLocaleDateString()}.</h2>
+    </div>
   );
 }
 export default App;
